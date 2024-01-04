84ba79be375c37f696a9c7043a4038f03152b2fb
Omar1s <122808487+Omar1s@users.noreply.github.com>
2024-01-04 13:45:32 +0200

    Update A3_SheetPb05_20220225.cpp

--- Task-1/A3_SheetPb05_20220225.cpp
+++ Task-1/A3_SheetPb05_20220225.cpp
@@ -1,8 +1,3 @@
-// File: A3_SheetPb05_20220225.cpp
-// Author: Omar Saeed Othman
-// Section: S(1)
-// ID: 20220225
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,70 +6,131 @@
 #include <psapi.h>
 #include <tlhelp32.h>
 #include <iomanip>
+#include <fstream>
+
 using namespace std;
+
 class Process {
 private:
     string name;
-    DWORD pid;
+    string session_name;
+    int pid;
+    int session_num;
     int memoryUsage;
-public:
-    Process(const string& name, DWORD pid, int memoryUsage): name(name), pid(pid), memoryUsage(memoryUsage) {}
 
+public:
+    Process(const string& name, string session_name, int pid, int memoryUsage, int session_num) :
+            name(name), session_name(session_name), session_num(session_num), pid(pid), memoryUsage(memoryUsage) {}
 
     string getName() const {
         return name;
     }
 
-    DWORD getPID() const {
+    string getSessionName() const {
+        return session_name;
+    }
+
+    int getSessionNum() const {
+        return session_num;
+    }
+
+    int getPID() const {
         return pid;
     }
 
     int getMemoryUsage() const {
         return memoryUsage;
     }
-
-
 };
 
 class ProcessList {
 private:
     vector<Process> processes;
+
 public:
     void fill() {
-        HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-        if (hProcessSnap == INVALID_HANDLE_VALUE) {
-            cerr << "Failed to create process snapshot." << endl;
-            return;
-        }
+        system("tasklist>input.txt");
 
-        PROCESSENTRY32 pe32;
-        pe32.dwSize = sizeof(PROCESSENTRY32);
+        ifstream inputFile("input.txt");
+        string line;
 
-        if (!Process32First(hProcessSnap, &pe32)) {
-            cerr << "Failed to get the first process." << endl;
-            CloseHandle(hProcessSnap);
-            return;
-        }
+        // Skip the first two lines as they contain headers
+        getline(inputFile, line);
+        getline(inputFile, line);
+        getline(inputFile, line);
 
-        processes.clear();
+        string word = "";
+        int flag = 0;
+        string name1;
+        string s_name;
+        int pid1;
+        int s_num;
+        int mem;
 
-        do {
-            int memoryUsage = 0;
-            HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pe32.th32ProcessID);
-            if (hProcess != NULL) {
+        while (getline(inputFile, line)) {
+            name1.clear(); // Clear the name1 variable
 
-                PROCESS_MEMORY_COUNTERS pmc;
-                if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc))) {
-                    memoryUsage = pmc.WorkingSetSize;
+            int flag;
+            for (int i = 0; i < line.size(); ++i) {
+                if (line[i] == ' ' && line[i + 1] == ' ') {
+                    flag = i;
+                    break;
+                } else {
+                    word += line[i];
                 }
-                CloseHandle(hProcess);
             }
 
-            Process newProcess(pe32.szExeFile, pe32.th32ProcessID, memoryUsage);
-            processes.push_back(newProcess);
-        } while (Process32Next(hProcessSnap, &pe32));
+            name1 = word;
+            word.clear();
 
-        CloseHandle(hProcessSnap);
+            for (int i = flag; i < line.size(); ++i) {
+                if (line[i] >= 'a' && line[i] <= 'z' || line[i] >= 'A' && line[i] <= 'Z') {
+                    flag = i;
+                    break;
+                } else if (line[i] >= '0' && line[i] <= '9') {
+                    word += line[i];
+                }
+            }
+            pid1 = stoi(word);
+            word.clear();
+
+            for (int i = flag; i < line.size(); ++i) {
+                if (line[i] == ' ' && line[i + 1] == ' ') {
+                    flag = i;
+                    break;
+                } else {
+                    word += line[i];
+                }
+            }
+            s_name = word;
+            word.clear();
+
+            for (int i = flag; i < line.size(); ++i) {
+                if (!word.empty() && line[i] == ' ') {
+                    flag = i;
+                    break;
+                } else if (line[i] >= '0' && line[i] <= '9') {
+                    word += line[i];
+                }
+            }
+            s_num = stoi(word);
+            word.clear();
+
+            for (int i = flag; i < line.size(); ++i) {
+                if (line[i] >= '0' && line[i] <= '9') {
+                    word += line[i];
+                } else if (line[i] == 'K') {
+                    break;
+                }
+            }
+            mem = stoi(word);
+            word.clear();
+
+            Process newProcess(name1, s_name, pid1, mem, s_num);
+            processes.push_back(newProcess);
+        }
+
+        inputFile.close();
     }
 
     void sortBy(int sortBy) {
@@ -82,13 +138,11 @@ public:
             sort(processes.begin(), processes.end(), [](const Process& a, const Process& b) {
                 return a.getName() < b.getName();
             });
-        }
-        else if (sortBy == 2) {
+        } else if (sortBy == 2) {
             sort(processes.begin(), processes.end(), [](const Process& a, const Process& b) {
                 return a.getPID() < b.getPID();
             });
-        }
-        else if (sortBy == 3) {
+        } else if (sortBy == 3) {
             sort(processes.begin(), processes.end(), [](const Process& a, const Process& b) {
                 return a.getMemoryUsage() > b.getMemoryUsage();
             });
@@ -96,20 +150,21 @@ public:
     }
 
     void display() const {
-        cout << "--------------------------------------------------------------------" << endl;
-        cout << "| Process Name                | PID      | Memory Usage (KB)    |" << endl;
-        cout << "--------------------------------------------------------------------" << endl;
+        cout << "====================================================================================================" << endl;
+
+        cout<<"| Process Name                 | PID      |Session Name     |Session_#       | Memory Usage (KB)    |" << endl;
+        cout << "====================================================================================================" << endl;
 
         for (const auto& process : processes) {
-            cout << "| " << left << std::setw(28) << process.getName() << " | "
-                      << setw(8) << process.getPID() << " | "
-                      << setw(20) << process.getMemoryUsage()/1024<<" KB" << " |" << endl;
+            cout << "| " << left << setw(28) << process.getName() << " | "
+                 << setw(8) << process.getPID() << " | "
+                 << setw(15) << process.getSessionName() << " | "
+                 << setw(14) << process.getSessionNum() << " | "
+                 << setw(20) << process.getMemoryUsage() << " KB" << " |" << endl;
         }
 
-        cout << "--------------------------------------------------------------------" <<endl;
+        cout << "========================================================================================================" << endl;
     }
-
-
 };
 
 int main() {
@@ -118,12 +173,12 @@ int main() {
 
     int sortBy;
     while (true) {
-        cout<<"Menu :\n";
-        cout<<"Welcome To Task Manger Please Enter :\n";
-        cout<<"1.Sort By Name\n";
-        cout<<"2.Sort By PID\n";
-        cout<<"3.Sort By Memory Usage \n";
-        cout<<"0.EXIT\n";
+        cout << "Menu :\n";
+        cout << "Welcome To Task Manager, Please Enter:\n";
+        cout << "1. Sort By Name\n";
+        cout << "2. Sort By PID\n";
+        cout << "3. Sort By Memory Usage\n";
+        cout << "0. EXIT\n";
 
         cin >> sortBy;
 
